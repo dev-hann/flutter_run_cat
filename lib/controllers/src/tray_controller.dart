92098af9e0bf7f1affccc1612a294b2bbc6f364c
yoehwan <yoehwan@gmail.com>
2022-05-01 21:03:31 +0800

    fix native code hide init

--- lib/controllers/src/tray_controller.dart
+++ lib/controllers/src/tray_controller.dart
@@ -3,15 +3,9 @@ part of controllers;
 class TrayController extends Controller {
   final TrayView _trayView = TrayView();
 
-  void onInit() {
-    print("@#@##@#");
-    onReady();
-  }
-
   @override
   void onReady() {
     super.onReady();
-    print("@#@#@#@#12122121");
     _trayView.init();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 import 'controllers/controllers.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  initControllers();  
+  initControllers();
+  runApp(const MyApp());
 }
 
-
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+        /// Add Setting View
+      home: SizedBox(),
+    );
+  }
+}

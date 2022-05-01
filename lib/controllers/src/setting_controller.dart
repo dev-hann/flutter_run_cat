03fc9b932bc16121842561fe9ef39b797c2257f3
dev-hann <developer.hann@gmail.com>
2022-05-01 23:38:56 +0800

    add cat icon

--- lib/controllers/src/setting_controller.dart
+++ lib/controllers/src/setting_controller.dart
@@ -3,8 +3,18 @@ part of controllers;
 class SettingController extends Controller {
   final SettingUseCase useCase = SettingUseCase(SettingImpl());
 
+  void _initWindow() {
+    doWhenWindowReady(() {
+      const initialSize = Size(600, 450);
+      appWindow.minSize = initialSize;
+      appWindow.size = initialSize;
+      appWindow.hide();
+    });
+  }
+
   @override
   void onReady() async {
+    _initWindow();
     await useCase.init();
     super.onReady();
   }
--- lib/controllers/src/tray_controller.dart
+++ lib/controllers/src/tray_controller.dart
@@ -7,17 +7,48 @@ class TrayController extends Controller {
   @override
   void onReady() async {
     super.onReady();
+    await _trayView.init();
+    await _initTray();
+    initTicker();
+  }
+
+  Future _initTray() async {
     await _tray.initSystemTray(
       title: "runCat",
       iconPath: 'assets/cat/0.png',
     );
-    _tray.setContextMenu(menu);
-    _trayView.init();
+    _tray.setContextMenu([
+      MenuItem(label: 'Show', onClicked: onTapShow),
+      MenuItem(label: 'Hide', onClicked: onTapHide),
+      MenuItem(label: 'Exit', onClicked: () {}),
+    ]);
   }
 
-  final menu = [
-    MenuItem(label: 'Show', onClicked: () {}),
-    MenuItem(label: 'Hide', onClicked: () {}),
-    MenuItem(label: 'Exit', onClicked: () {}),
-  ];
+  void onTapShow() {
+    appWindow.show();
+  }
+
+  void onTapHide() {
+    appWindow.hide();
+  }
+
+  void onChangedSetting(Setting setting) {}
+
+  /// Ticker
+  Timer? _timer;
+  void initTicker(
+      // String dir,
+      // int speed,
+      ) {
+    if (_timer != null && _timer!.isActive) {
+      _timer!.cancel();
+    }
+    _timer = Timer.periodic(const Duration(milliseconds: 300), onTick);
+  }
+
+  void onTick(Timer timer) async {
+    final index = timer.tick % 5;
+    print(index);
+    await _tray.setImage('assets/cat/$index.png');
+  }
 }

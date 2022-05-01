8b5f74c00539c598f611b45d25be32972f10c457
dev-hann <developer.hann@gmail.com>
2022-05-01 21:21:52 +0800

    add window handler

--- lib/controllers/controllers.dart
+++ lib/controllers/controllers.dart
@@ -7,6 +7,7 @@ import 'package:flutter_run_cat/repos/setting_repo/setting_impl.dart';
 import 'package:flutter_run_cat/use_cases/setting_use_case/setting_use_case.dart';
 import 'package:flutter_run_cat/views/tray_view/tray_view.dart';
 import 'package:get/get.dart';
+import 'package:system_tray/system_tray.dart';
 
 part 'src/setting_controller.dart';
 part 'src/system_controller.dart';
@@ -26,11 +27,7 @@ abstract class Controller extends GetxController {
 }
 
 Future initControllers() async {
-  print("!!1");
   Get.put(SettingController());
-  print("!!2");
   Get.put(SystemController());
-  print("!!3");
   Get.put(TrayController());
-  print("!!5");
 }

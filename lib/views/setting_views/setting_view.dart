ce78733ff46d5c23e352ac268a56c624c090e9df
dev-hann <developer.hann@gmail.com>
2022-05-04 21:32:12 +0800

    update: setting view:

--- lib/views/setting_views/setting_view.dart
+++ lib/views/setting_views/setting_view.dart
@@ -1,18 +1,49 @@
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 
+import 'menu_view.dart';
+
 class SettingView extends StatelessWidget {
   const SettingView({Key? key}) : super(key: key);
+
+  Widget _appBar() {
+    return WindowTitleBarBox(
+      child: Row(
+        children: [
+          Expanded(
+            child: MoveWindow(
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Run Cat"),
+              ),
+            ),
+          ),
+          CloseWindowButton(
+            onPressed: () {
+              appWindow.close();
+              appWindow.hide();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WindowBorder(
-        color: Colors.red,
-        child: Center(
-          child: Text("!!!"),
+        color: Colors.blue,
+        width: 2,
+        child: Column(
+          children: [
+            _appBar(),
+            MenuView(),
+            Divider(),
+            Text("@#@#@:w"),
+          ],
         ),
       ),
-    
+    );
   }
 }

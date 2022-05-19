daacc60445567891ab3c0e95ee225ca1da3388f4
yoehwan <yoehwan@gmail.com>
2022-05-19 08:45:30 +0800

    "update:

--- install.sh
+++ install.sh
@@ -1,18 +1,61 @@
-#!usr/bin/env bash
+#!/bin/bash
 
 
 echo "Start Update..."
 APP_NAME="flutter_run_cat"
 
-                                                                  
-                                                                  
+RELEASE="https://github.com/yoehwan/flutter_run_cat/releases/download/v1.0.0/"
+
+RELEASE_NAME="Run.Cat.v1.0.0.x64.linux.tar.xz"
+
+OUTPUT="$HOME/flutter_run_cat"
+
+
 # ShutDown app
 pkill $APP_NAME
 
+download()
+{
+if [ -x "$(which wget)" ] ; then
+	echo "download wget."
+	wget $RELEASE$RELEASE_NAME
+elif [ -x "$(which curl)" ] ; then
+	echo "download curl."
+else 
+	echo "Could not find curl or wget, please install one."
+fi
+}
+
+unzip()
+{
+
+mkdir -p $OUTPUT
+
+tar -xf $RELEASE_NAME -C "$OUTPUT"
+
+}
+
+addPath()
+{
+	
+	VAR="FLUTTER_RUN_CAT"
+	EXPORT="export $VAR=\"$OUTPUT\""
+	BASHRC="$HOME/.bashrc"
+
+	if ! grep -q "$EXPORT" "$BASHRC" ; then
+		echo "$EXPORT" >> "$BASHRC"
+
+		#fix here
+		echo "export PATH=\$PATH:\$$VAR/$APP_NAME" >> "$BASHRC"
+	fi
+	
+}
 
 
+#download
 
+#unzip
 
-
+addPath
 
 
